3c8e3bd955a3fbc109b81674b016138268741d8e
Steven Xia <stevenxia@protonmail.com>
2021-05-06 15:13:22 -0700

    Fix oversight in graph update script.

--- update_charts.sh
+++ update_charts.sh
@@ -1,5 +1,5 @@
 git fetch --all && git reset --hard origin/master
 python3 main.py
-git diff --exit-code --quiet data/output_tourney.png || git add output_tourney.png
-git diff --exit-code --quiet data/output_bar.png || git add output_bar.png output_line.png
+git diff --exit-code --quiet data/output_tourney.png || git add data/output_tourney.png
+git diff --exit-code --quiet data/output_bar.png || git add data/output_bar.png data/output_line.png
 git commit -m "Update rating charts for `date '+%Y-%m-%d %H:00:00'`." && git push
