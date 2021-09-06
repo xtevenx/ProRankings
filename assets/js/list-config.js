33e8b206feacbb936a348fda380043427e85130f
Steven Xia <stevenxia@protonmail.com>
2021-09-05 21:27:21 -0700

    Add league filter for rating table.

---
+++ assets/js/list-config.js
@@ -0,0 +1,15 @@
+const ratingTableCheckboxes = [
+    document.getElementById("rating-table-lcs"),
+    document.getElementById("rating-table-lcs"),
+    document.getElementById("rating-table-lec"),
+    document.getElementById("rating-table-lck"),
+    document.getElementById("rating-table-lpl"),
+    document.getElementById("rating-table-cblol"),
+    document.getElementById("rating-table-lcl"),
+    document.getElementById("rating-table-ljl"),
+    document.getElementById("rating-table-lla"),
+    document.getElementById("rating-table-lco"),
+    document.getElementById("rating-table-pcs"),
+    document.getElementById("rating-table-tcl"),
+    document.getElementById("rating-table-vcs"),
+];
