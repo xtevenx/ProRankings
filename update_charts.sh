7dda6cabb052d9d35cc7f808e725d7e16022315f
Steven Xia <stevenxia@protonmail.com>
2021-09-05 22:41:14 -0700

    Update auto-update script.

--- update_charts.sh
+++ update_charts.sh
@@ -2,5 +2,5 @@ git fetch --all && git reset --hard origin/master
 python3 -m pip install -r requirements.txt --user || python3 -m pip install -r requirements.txt
 python3 main.py
 git add data/output_bar.png data/output_tourney.png data/output_line.png
-git add README.md README.html
+git add README.md README.html assets/js/chart-config.js
 git commit -m "Update rating charts for `date '+%Y-%m-%d %H:00:00'`." && git push
