02a1d48c162732019f0cb244e1dde31899f8010a
xnevetx <fuwasteven@gmail.com>
2022-05-11 11:00:34 +0000

    Update rating charts for 2022-05-11 11:00:00.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -3,10 +3,10 @@ const MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "A
 const topChartConfig = {
     type: "bar",
     data: {
-        labels: ['T1', 'G2 Esports', 'Gen.G', 'DWG KIA', 'Royal Never Give Up', 'Top Esports', 'Victory Five', 'JD Gaming', 'Evil Geniuses.NA', 'EDward Gaming', 'Kwangdong Freecs', 'Rogue (European Team)'],
+        labels: ['T1', 'G2 Esports', 'Gen.G', 'DWG KIA', 'Royal Never Give Up', 'Top Esports', 'Victory Five', 'Evil Geniuses.NA', 'JD Gaming', 'EDward Gaming', 'Kwangdong Freecs', 'Rogue (European Team)'],
         datasets: [{
             backgroundColor: ['#dc5f57', '#dca157', '#d4dc57', '#91dc57', '#57dc5f', '#57dca1', '#57d4dc', '#5791dc', '#5f57dc', '#a157dc', '#dc57d4', '#dc5791'],
-            data: [2540.8523906925675, 2399.3627925327078, 2387.898616079233, 2372.332289879694, 2364.9107513325644, 2336.9448755164985, 2303.111970811392, 2258.759450606141, 2248.881306281909, 2238.41758265801, 2238.069224561753, 2235.074396943037],
+            data: [2540.8523906925675, 2399.3627925327078, 2387.898616079233, 2372.332289879694, 2364.9107513325644, 2336.9448755164985, 2303.111970811392, 2266.0350917037536, 2258.759450606141, 2238.41758265801, 2238.069224561753, 2235.074396943037],
             label: "Rating Points"
         }]
     },
