b96077736cd52b49dd4717609b35e3b7328289e5
xnevetx <fuwasteven@gmail.com>
2022-05-24 15:06:17 -0700

    Add git repository setup and other tweaks to auto-update script.

--- update_charts.sh
+++ update_charts.sh
@@ -1,5 +1,6 @@
+git remote set-url origin git@github.com:xtevenx/ProRankings
 git fetch --all && git reset --hard origin/master
 python3 -m pip install -r requirements.txt --user || python3 -m pip install -r requirements.txt
 python3 main.py
 git add index.html assets/js/chart-config.js
-git commit -m "Update rating charts for `date '+%Y-%m-%d %H:00:00'`." && git push
+git commit -m "Update rating charts for `date '+%Y-%m-%d %T'`." && git push
