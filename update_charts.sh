9ca93378688a7334929cec0969fdc158ef6b3bfa
Steven Xia <stevenxia@protonmail.com>
2022-07-06 15:46:34 -0700

    Don't attempt to commit if there is nothing.

--- update_charts.sh
+++ update_charts.sh
@@ -3,4 +3,4 @@ git fetch --all && git reset --hard origin/master
 python3 -m pip install -r requirements.txt --user || python3 -m pip install -r requirements.txt
 python3 main.py
 git add index.html assets/js/chart-config.js
-git commit -m "Update rating charts for `date '+%Y-%m-%d %T'`." && git push
+git diff-index --quiet origin/master || git commit -m "Update rating charts for `date '+%Y-%m-%d %T'`." && git push
