2c65090963bffe88ebd514975ac8a27c813dc7e2
Steven Xia <stevenxia@protonmail.com>
2022-07-07 22:16:35 -0700

    Update update script's repo updating. :D

--- update_charts.sh
+++ update_charts.sh
@@ -1,6 +1,7 @@
-git remote set-url origin git@github.com:xtevenx/ProRankings
-git fetch --all && git reset --hard origin/master
-python3 -m pip install -r requirements.txt --user || python3 -m pip install -r requirements.txt
+git fetch --depth=1
+git reset --hard origin/master
+python3 -m pip install -r requirements.txt
 python3 main.py
 git add index.html assets/js/chart-config.js
-git diff-index --quiet origin/master || git commit -m "Update rating charts for `date '+%Y-%m-%d %T'`." && git push
+git diff-index --quiet origin/master || git commit -m "Update rating charts for `date '+%Y-%m-%d %T'`."
+git push
