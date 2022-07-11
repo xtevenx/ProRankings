642a3048032dd5c0bc61764ecf5ab48e7b35fa61
Steven Xia <stevenxia@protonmail.com>
2022-07-11 15:12:29 -0700

    Save data for future use.
    should reduce aws costs! :D
    just remember to delete this every once in a while just in case it's not robust.

--- UPDATE.md
+++ UPDATE.md
@@ -6,5 +6,6 @@ Things to manually update
 * `pro_rankings.py`: Pro league names for API access.
 * `pro_rankings.py`: Season end dates (except they're actually start dates).
 * `TEMPLATE.html`: Pro league wiki links.
+* `data/past_data.json`: Delete this every once in a ~~while~~ season.
 
 *Note: These are also possible targets for automation.*
--- update_charts.sh
+++ update_charts.sh
@@ -1,7 +1,7 @@
-git fetch --depth=1
+git pull --depth=1
 git reset --hard origin/master
 python3 -m pip install -r requirements.txt
 python3 main.py
-git add index.html assets/js/chart-config.js
+git add index.html assets/js/chart-config.js data/past_data.json
 git diff-index --quiet origin/master || git commit -m "Update rating charts for `date '+%Y-%m-%d %T'`."
 git push
