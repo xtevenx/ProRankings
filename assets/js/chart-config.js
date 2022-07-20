19df84f6ba5787d6feefa4725fd0ebb64d5b587d
xnevetx <fuwasteven@gmail.com>
2022-07-20 12:36:42 +0000

    Update rating charts for 2022-07-20 12:36:42.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Gen.G', 'T1', 'DWG KIA', 'Royal Never Give Up', 'Top Esports', 'Victory Five', 'Evil Geniuses.NA', 'Rogue (European Team)', 'Liiv SANDBOX', 'JD Gaming', 'KT Rolster', 'DRX'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2542.2, 2533.8, 2433.4, 2418.9, 2391.3, 2386.2, 2317.1, 2291.0, 2287.9, 2267.7, 2256.8, 2253.4],
+            data: [2542.2, 2533.8, 2437.4, 2418.9, 2391.3, 2386.2, 2317.1, 2291.0, 2287.9, 2267.7, 2256.8, 2253.4],
             label: "Rating Points"
         }]
     },
