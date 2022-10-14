a86d77be803fc4077c2b5cae2cff3370fb03e003
xnevetx <fuwasteven@gmail.com>
2022-10-14 00:36:48 +0000

    Update rating charts for 2022-10-14 00:36:48.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -3,10 +3,10 @@ const MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "A
 const topChartConfig = {
     type: "bar",
     data: {
-        labels: ['Gen.G', 'JD Gaming', 'T1', 'EDward Gaming', 'Royal Never Give Up', 'DWG KIA', 'Rogue (European Team)', 'DRX', 'Top Esports', 'Liiv SANDBOX', 'KT Rolster', 'Victory Five'],
+        labels: ['Gen.G', 'JD Gaming', 'T1', 'Royal Never Give Up', 'DWG KIA', 'EDward Gaming', 'Rogue (European Team)', 'DRX', 'Top Esports', 'Liiv SANDBOX', 'KT Rolster', 'Victory Five'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2544.1, 2493.9, 2438.6, 2403.7, 2402.0, 2387.2, 2366.3, 2361.0, 2326.3, 2314.9, 2283.6, 2267.9],
+            data: [2544.1, 2493.9, 2466.4, 2402.0, 2387.2, 2379.1, 2366.3, 2361.0, 2326.3, 2314.9, 2283.6, 2267.9],
             label: "Rating Points"
         }]
     },
