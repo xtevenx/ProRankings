aa9f9a4691a1c836dd8dc9d691c54fbb67aaf35a
xnevetx <fuwasteven@gmail.com>
2023-01-29 11:36:41 +0000

    Update rating charts for 2023-01-29 11:36:41.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['T1', 'Gen.G', 'DWG KIA', 'JD Gaming', 'DRX', 'Liiv SANDBOX', 'Top Esports', 'EDward Gaming', 'KT Rolster', 'Weibo Gaming', 'Bilibili Gaming', 'Victory Five'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2600.5, 2475.4, 2465.9, 2382.0, 2347.7, 2317.9, 2313.5, 2307.6, 2305.9, 2287.2, 2284.2, 2267.9],
+            data: [2600.5, 2475.4, 2465.9, 2382.0, 2359.8, 2317.9, 2313.5, 2307.6, 2305.9, 2287.2, 2284.2, 2267.9],
             label: "Rating Points"
         }]
     },
