a3c8a175c4df7b6b58521716a25e7677d7661567
xnevetx <fuwasteven@gmail.com>
2023-05-14 13:36:52 +0000

    Update rating charts for 2023-05-14 13:36:52.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['T1', 'JD Gaming', 'Gen.G', 'KT Rolster', 'Bilibili Gaming', 'Dplus KIA', 'G2 Esports', 'Hanwha Life Esports', 'EDward Gaming', 'Liiv SANDBOX', 'LNG Esports', 'Golden Guardians'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2580.3, 2541.1, 2520.0, 2443.8, 2382.6, 2352.1, 2336.6, 2336.6, 2308.4, 2280.4, 2267.8, 2261.6],
+            data: [2580.3, 2559.0, 2520.0, 2443.8, 2369.8, 2352.1, 2336.6, 2336.6, 2308.4, 2280.4, 2267.8, 2261.6],
             label: "Rating Points"
         }]
     },
