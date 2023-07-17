bce791ba1769a5b7d96beb46f49e0396f0fd7c00
xnevetx <fuwasteven@gmail.com>
2023-07-17 19:36:57 +0000

    Update rating charts for 2023-07-17 19:36:57.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Gen.G', 'JD Gaming', 'KT Rolster', 'Bilibili Gaming', 'Dplus KIA', 'T1', 'Hanwha Life Esports', 'G2 Esports', 'LNG Esports', 'Top Esports', 'Weibo Gaming', 'Oh My God'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2569.9, 2540.1, 2521.7, 2489.1, 2424.5, 2387.5, 2364.6, 2349.7, 2325.2, 2305.7, 2276.5, 2248.7],
+            data: [2569.9, 2540.1, 2521.7, 2489.1, 2424.5, 2387.5, 2364.6, 2359.1, 2325.2, 2305.7, 2276.5, 2248.7],
             label: "Rating Points"
         }]
     },
