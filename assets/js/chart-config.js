8bfef4baf19e25a0ea81a27c8eda18c9e8ff07d2
xnevetx <fuwasteven@gmail.com>
2024-02-25 14:37:16 +0000

    Update rating charts for 2024-02-25 14:37:16.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -3,10 +3,10 @@ const MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "A
 const topChartConfig = {
     type: "bar",
     data: {
-        labels: ['T1', 'Gen.G', 'Bilibili Gaming', 'Top Esports', 'JD Gaming', 'Hanwha Life Esports', 'KT Rolster', 'G2 Esports', 'Dplus KIA', 'Weibo Gaming', 'Ninjas in Pyjamas.CN', 'CTBC Flying Oyster'],
+        labels: ['T1', 'Gen.G', 'Bilibili Gaming', 'Top Esports', 'JD Gaming', 'Hanwha Life Esports', 'KT Rolster', 'G2 Esports', 'Dplus KIA', 'Ninjas in Pyjamas.CN', 'Weibo Gaming', 'CTBC Flying Oyster'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2647.3, 2601.1, 2529.6, 2525.9, 2486.7, 2478.0, 2421.4, 2396.5, 2370.6, 2334.1, 2327.5, 2306.7],
+            data: [2647.3, 2601.1, 2529.6, 2525.9, 2486.7, 2478.0, 2421.4, 2396.5, 2370.6, 2337.8, 2334.1, 2306.7],
             label: "Rating Points"
         }]
     },
