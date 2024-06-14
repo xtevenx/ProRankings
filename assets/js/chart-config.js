7a97ed96455d60cf6fc22082825fec5e28d2bbb4
xnevetx <fuwasteven@gmail.com>
2024-06-14 13:37:10 +0000

    Update rating charts for 2024-06-14 13:37:10.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Gen.G', 'Bilibili Gaming', 'T1', 'Top Esports', 'Hanwha Life Esports', 'G2 Esports', 'JD Gaming', 'Dplus KIA', 'PSG Talon', 'Team WE', 'Ninjas in Pyjamas.CN', 'KT Rolster'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2702.5, 2569.7, 2566.3, 2511.8, 2474.3, 2449.9, 2419.9, 2414.7, 2343.4, 2311.7, 2287.7, 2272.5],
+            data: [2702.5, 2569.7, 2568.3, 2511.8, 2474.3, 2449.9, 2419.9, 2414.7, 2343.4, 2311.7, 2287.7, 2272.5],
             label: "Rating Points"
         }]
     },
