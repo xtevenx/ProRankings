abe8b1e43d3f9730133c61b0c4febad4ce05d51e
xnevetx <fuwasteven@gmail.com>
2024-06-19 11:37:05 +0000

    Update rating charts for 2024-06-19 11:37:05.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -3,10 +3,10 @@ const MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "A
 const topChartConfig = {
     type: "bar",
     data: {
-        labels: ['Bilibili Gaming', 'Top Esports', 'G2 Esports', 'JD Gaming', 'PSG Talon', 'Team WE', 'Team Liquid', 'LNG Esports', 'CTBC Flying Oyster', 'Fnatic', "Anyone's Legend", 'Team BDS'],
+        labels: ['Bilibili Gaming', 'Top Esports', 'G2 Esports', 'JD Gaming', 'PSG Talon', 'Team WE', 'Team Liquid', 'LNG Esports', 'CTBC Flying Oyster', 'Fnatic', 'Ninjas in Pyjamas.CN', "Anyone's Legend"],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2580.1, 2518.4, 2453.0, 2440.8, 2354.4, 2294.3, 2279.6, 2272.4, 2270.7, 2253.9, 2246.9, 2230.2],
+            data: [2580.1, 2518.4, 2453.0, 2440.8, 2354.4, 2294.3, 2279.6, 2272.4, 2270.7, 2253.9, 2247.7, 2246.9],
             label: "Rating Points"
         }]
     },
