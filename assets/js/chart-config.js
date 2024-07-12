6bc4eb2a6b8d6c179bc2e733918dee37aa08f615
xnevetx <fuwasteven@gmail.com>
2024-07-12 12:37:09 +0000

    Update rating charts for 2024-07-12 12:37:09.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -3,10 +3,10 @@ const MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "A
 const topChartConfig = {
     type: "bar",
     data: {
-        labels: ['Gen.G', 'Top Esports', 'T1', 'Bilibili Gaming', 'Hanwha Life Esports', 'G2 Esports', 'Dplus KIA', 'JD Gaming', "Anyone's Legend", 'PSG Talon', 'Team Liquid', 'KT Rolster'],
+        labels: ['Gen.G', 'Top Esports', 'T1', 'Bilibili Gaming', 'Hanwha Life Esports', 'G2 Esports', 'Dplus KIA', 'PSG Talon', 'JD Gaming', "Anyone's Legend", 'Team Liquid', 'KT Rolster'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2696.2, 2553.3, 2535.7, 2516.8, 2471.2, 2431.0, 2426.1, 2390.3, 2370.9, 2362.6, 2340.1, 2323.9],
+            data: [2696.2, 2553.3, 2521.6, 2516.8, 2500.1, 2431.0, 2426.1, 2391.0, 2390.3, 2370.9, 2340.1, 2330.1],
             label: "Rating Points"
         }]
     },
