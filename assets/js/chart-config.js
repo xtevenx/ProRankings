3822a508ef523e563b0556348f9362cc315c6a0d
xnevetx <fuwasteven@gmail.com>
2024-08-23 11:37:09 +0000

    Update rating charts for 2024-08-23 11:37:09.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Gen.G', 'Bilibili Gaming', 'Hanwha Life Esports', 'Top Esports', 'PSG Talon', 'Dplus KIA', 'T1', 'G2 Esports', 'Team Liquid', 'Weibo Gaming', 'LNG Esports', 'KT Rolster'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2706.3, 2562.4, 2550.0, 2545.3, 2489.1, 2475.2, 2450.5, 2377.8, 2353.6, 2332.5, 2323.4, 2321.1],
+            data: [2706.3, 2562.4, 2550.0, 2545.3, 2499.1, 2453.1, 2450.5, 2377.8, 2353.6, 2332.5, 2323.4, 2321.1],
             label: "Rating Points"
         }]
     },
