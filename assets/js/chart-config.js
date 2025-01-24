60ebebcc8c4568f27cc8c53b8e3482d40eba4a45
xnevetx <fuwasteven@gmail.com>
2025-01-24 14:37:12 +0000

    Update rating charts for 2025-01-24 14:37:12.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Bilibili Gaming', 'Gen.G', 'Top Esports', 'T1', 'Hanwha Life Esports', 'FlyQuest', 'Dplus KIA', 'Weibo Gaming', "Anyone's Legend", 'PSG Talon', 'ThunderTalk Gaming', 'LNG Esports'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2625.6, 2550.2, 2496.8, 2485.0, 2485.0, 2451.8, 2432.4, 2409.3, 2377.9, 2353.7, 2352.8, 2326.0],
+            data: [2625.6, 2550.2, 2496.8, 2495.3, 2485.0, 2451.8, 2432.4, 2409.3, 2377.9, 2353.7, 2352.8, 2326.0],
             label: "Rating Points"
         }]
     },
