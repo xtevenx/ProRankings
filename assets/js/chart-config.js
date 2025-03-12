98967de5f59f332ad856fcbd4ce998f8b55de4d6
xnevetx <fuwasteven@gmail.com>
2025-03-12 12:37:18 +0000

    Update rating charts for 2025-03-12 12:37:18.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Hanwha Life Esports', 'T1', 'Bilibili Gaming', 'Gen.G', 'Top Esports', "Anyone's Legend", 'Dplus KIA', 'CTBC Flying Oyster', 'JD Gaming', 'ThunderTalk Gaming', 'Deep Cross Gaming', 'Weibo Gaming'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2593.3, 2527.0, 2518.4, 2517.5, 2510.6, 2432.0, 2422.7, 2412.5, 2401.6, 2390.7, 2386.5, 2358.7],
+            data: [2593.3, 2527.0, 2518.4, 2517.5, 2490.3, 2432.0, 2422.7, 2412.5, 2401.6, 2390.7, 2386.5, 2358.7],
             label: "Rating Points"
         }]
     },
