e29b480f645c6331a8d297ac9de4cc185967b6c3
xnevetx <fuwasteven@gmail.com>
2025-05-17 21:37:14 +0000

    Update rating charts for 2025-05-17 21:37:14.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -3,10 +3,10 @@ const MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "A
 const topChartConfig = {
     type: "bar",
     data: {
-        labels: ['Gen.G', 'Hanwha Life Esports', 'CTBC Flying Oyster', 'Top Esports', "Anyone's Legend", 'T1', 'Bilibili Gaming', 'JD Gaming', 'FlyQuest', 'Invictus Gaming', 'PSG Talon', 'Cloud9'],
+        labels: ['Gen.G', 'Hanwha Life Esports', 'CTBC Flying Oyster', 'Top Esports', "Anyone's Legend", 'T1', 'Bilibili Gaming', 'FlyQuest', 'JD Gaming', 'Invictus Gaming', 'PSG Talon', 'Dplus KIA'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2642.8, 2555.0, 2498.8, 2494.9, 2459.4, 2450.7, 2432.9, 2421.4, 2378.4, 2375.2, 2353.7, 2348.2],
+            data: [2642.8, 2555.0, 2498.8, 2494.9, 2459.4, 2450.7, 2432.9, 2424.1, 2421.4, 2375.2, 2353.7, 2337.1],
             label: "Rating Points"
         }]
     },
