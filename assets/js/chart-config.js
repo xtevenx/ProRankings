f2986ef26e3769f1daa6b4d33223e6b618864594
xnevetx <fuwasteven@gmail.com>
2025-08-22 09:37:09 +0000

    Update rating charts for 2025-08-22 09:37:09.

--- assets/js/chart-config.js
+++ assets/js/chart-config.js
@@ -6,7 +6,7 @@ const topChartConfig = {
         labels: ['Gen.G', 'T1', 'Top Esports', "Anyone's Legend", 'Hanwha Life Esports', 'Invictus Gaming', 'FlyQuest', 'Bilibili Gaming', 'CTBC Flying Oyster', 'G2 Esports', 'JD Gaming', 'Weibo Gaming'],
         datasets: [{
             backgroundColor: ['#f07178', '#f78c6c', '#ffcb6b', '#ebd576', '#d7de82', '#c3e88d', '#a3c9c6', '#82aaff', '#a59ef5', '#c792ea', '#9c87cf', '#717cb4'],
-            data: [2666.3, 2634.0, 2507.8, 2505.6, 2494.5, 2475.7, 2456.1, 2451.1, 2445.4, 2353.4, 2347.3, 2324.9],
+            data: [2666.3, 2636.1, 2507.8, 2505.6, 2494.5, 2475.7, 2456.1, 2451.1, 2445.4, 2353.4, 2347.3, 2324.9],
             label: "Rating Points"
         }]
     },
